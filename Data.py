fae829eed48f0c8dc8f614001c2c8c56277daf4a
ElSenor64 <sillynoodle36@gmail.com>
2020-01-24 10:27:12 -0700

    Data Initialization start.

--- Data.py
+++ Data.py
@@ -1,7 +1,16 @@
 from os import * # Import bash run abilities
-
+from os.path import expanduser
+home = expanduser("~")
 import sys
 
 class DataManager():
     def __init__(self):
-        path = ~/.local/
+        if ("./monocle/" not in home+"/.local/share"):
+            system("mkdir ~/.local/share/monocle")
+
+        path = home+"/.local/share/monocle/"
+        notebooks = listdir(path)
+        for i in notebooks:
+            print (i)
+
+debug = DataManager()
